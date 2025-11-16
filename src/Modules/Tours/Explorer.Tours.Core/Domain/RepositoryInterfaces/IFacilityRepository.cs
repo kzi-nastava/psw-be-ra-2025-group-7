4f5a954652fe058e7bcd88d8006d03a0735c8d8d
Gagi232 <drganpolic32@gmail.com>
2025-11-16 23:32:14 +0100

    feat: add Facility entity, DTO, service, repository interface and AutoMapper config

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/RepositoryInterfaces/IFacilityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain.RepositoryInterfaces
+{
+    public interface IFacilityRepository
+    {
+        PagedResult<Facility> GetPaged(int page, int pageSize);
+        Facility Create(Facility facility);
+        Facility Update(Facility facility);
+        void Delete(int id);
+        Facility? GetById(int id);
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/FacilityService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.Domain;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases.Administration
+{
+    public class FacilityService : IFacilityService
+    {
+        private readonly IFacilityRepository _facilityRepository;
+        private readonly IMapper _mapper;
+
+        public FacilityService(IFacilityRepository facilityRepository, IMapper mapper)
+        {
+            _facilityRepository = facilityRepository;
+            _mapper = mapper;
+        }
+
+        public PagedResult<FacilityDto> GetPaged(int page, int pageSize)
+        {
+            var result = _facilityRepository.GetPaged(page, pageSize);
+            var items = result.Results.Select(_mapper.Map<FacilityDto>).ToList();
+            return new PagedResult<FacilityDto>(items, result.TotalCount);
+        }
+
+        public FacilityDto Create(FacilityDto facility)
+        {
+            var entity = _mapper.Map<Facility>(facility);
+            var result = _facilityRepository.Create(entity);
+            return _mapper.Map<FacilityDto>(result);
+        }
+
+        public FacilityDto Update(FacilityDto facility)
+        {
+            var entity = _mapper.Map<Facility>(facility);
+            var result = _facilityRepository.Update(entity);
+            return _mapper.Map<FacilityDto>(result);
+        }
+
+        public void Delete(int id)
+        {
+            _facilityRepository.Delete(id);
+        }
+    }
+}
