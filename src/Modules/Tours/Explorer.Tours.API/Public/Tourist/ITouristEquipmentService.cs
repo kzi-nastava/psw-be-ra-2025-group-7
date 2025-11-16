62f8c88ebad4c53afbae3dd447d8206711c91a7d
Dimitrije Ignjic <dimi3jeignjic@gmail.com>
2025-11-16 22:32:44 +0100

    feat: tourist equipment service and DTOs

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Tourist/ITouristEquipmentService.cs
@@ -0,0 +1,12 @@
+﻿using Explorer.Tours.API.Dtos;
+
+namespace Explorer.Tours.API.Public.Tourist;
+
+public interface ITouristEquipmentService
+{
+ 
+    List<TouristEquipmentDto> GetByTourist(long touristId);
+
+
+    List<TouristEquipmentDto> UpdateForTourist(UpdateTouristEquipmentDto request);
+}
