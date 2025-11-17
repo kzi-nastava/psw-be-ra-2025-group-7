5946a4af9111e18f81cb78bb5049e7979cebf9dd
BlagojeP <blagojepanic9@gmail.com>
2025-11-18 00:39:38 +0100

    test: add integration tests for account administration

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/c-insert-accounts.sql
@@ -0,0 +1,6 @@
+INSERT INTO "stakeholders"."Accounts"
+    ("Id", "Username", "Password", "Email", "Role", "IsBlocked")
+VALUES
+    (-1, 'test_author', 'test_password', 'author@test.com', 'Author', false),
+    (-2, 'test_tourist', 'test_password', 'tourist@test.com', 'Tourist', false),
+    (-3, 'test_admin', 'test_password', 'admin@test.com', 'Administrator', false);
