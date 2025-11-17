1f91f3411fd334a27a346cfe830127505bdc8111
BlagojeP <blagojepanic9@gmail.com>
2025-11-17 23:40:22 +0100

    feat: add account entity, service, repository, controller and database configuration

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/AccountRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.Stakeholders.Core.Domain;
+using Explorer.Stakeholders.Core.Domain.RepositoryInterfaces;
+using Explorer.Stakeholders.Infrastructure.Database;
+
+namespace Explorer.Stakeholders.Infrastructure.Database.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly StakeholdersContext _context;
+
+        public AccountRepository(StakeholdersContext context)
+        {
+            _context = context;
+        }
+
+        public Account Create(Account account)
+        {
+            _context.Accounts.Add(account);
+            _context.SaveChanges();
+            return account;
+        }
+
+        public IEnumerable<Account> GetAll()
+        {
+            return _context.Accounts.ToList();
+        }
+
+        public Account? Get(int id)
+        {
+            return _context.Accounts.Find(id);
+        }
+
+        public void Update(Account account)
+        {
+            _context.Accounts.Update(account);
+            _context.SaveChanges();
+        }
+    }
+}
