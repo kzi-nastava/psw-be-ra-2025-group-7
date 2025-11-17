3d96d69b2789f87bb3943571cad1fca6a94d87ec
Dimitrije Ignjic <dimi3jeignjic@gmail.com>
2025-11-17 16:29:43 +0100

    feat: add TouristEquipment EF repository and configure ToursContext

---
+++ src/Explorer.API/Controllers/Tourist/TouristEquipmentController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Tourist;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist;
+
+[ApiController]
+[Route("api/tourists/equipment")]
+[Authorize(Policy = "touristPolicy")]
+public class TouristEquipmentController : ControllerBase
+{
+    private readonly ITouristEquipmentService _service;
+
+    public TouristEquipmentController(ITouristEquipmentService service)
+    {
+        _service = service;
+    }
+
+    // GET api/tourists/equipment/{touristId}
+    [HttpGet("{touristId:long}")]
+    public ActionResult<List<TouristEquipmentDto>> GetByTourist(long touristId)
+    {
+        var result = _service.GetByTourist(touristId);
+        return Ok(result);
+    }
+
+    // PUT api/tourists/equipment
+    [HttpPut]
+    public ActionResult<List<TouristEquipmentDto>> Update(
+        [FromBody] UpdateTouristEquipmentDto request)
+    {
+        var result = _service.UpdateForTourist(request);
+        return Ok(result);
+    }
+}
