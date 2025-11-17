3cafb412881ead846f857836bfaebef87f251a4a
Gio <ognjen.vujanic03@gmail.com>
2025-11-17 20:46:35 +0100

    feat: Kreitanje klase TourProblem, dto,mapiranje, interfejsi i servisne klase

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -14,5 +14,11 @@ public class ToursProfile : Profile
         CreateMap<TourDto, Tour>().ReverseMap();
         CreateMap<TouristEquipment, TouristEquipmentDto>().ReverseMap();
 
+        CreateMap<TourProblemDto, TourProblem>()
+           .ForMember(d => d.Category, opt => opt.MapFrom(s => Enum.Parse<ProblemCategory>(s.Category, true)))
+           .ForMember(d => d.Priority, opt => opt.MapFrom(s => Enum.Parse<ProblemPriority>(s.Priority, true)));
+        CreateMap<TourProblem, TourProblemDto>()
+            .ForMember(d => d.Category, opt => opt.MapFrom(s => s.Category.ToString()))
+            .ForMember(d => d.Priority, opt => opt.MapFrom(s => s.Priority.ToString()));
     }
 }
