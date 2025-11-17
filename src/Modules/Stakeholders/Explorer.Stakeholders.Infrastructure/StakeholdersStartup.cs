350fe256e061cc9027b42724a5568f0aa5c23549
Teodora <teodora.lazarevic251103@gmail.com>
2025-11-17 20:45:21 +0100

    feat: add UserProfile repository and database infrastructure

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
@@ -26,12 +26,14 @@ public static class StakeholdersStartup
     {
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<ITokenGenerator, JwtGenerator>();
+        services.AddScoped<IUserProfileService, UserProfileService>();
     }
 
     private static void SetupInfrastructure(IServiceCollection services)
     {
         services.AddScoped<IPersonRepository, PersonDbRepository>();
         services.AddScoped<IUserRepository, UserDbRepository>();
+        services.AddScoped<IUserProfileRepository, UserProfileDbRepository>();
 
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(DbConnectionStringBuilder.Build("stakeholders"));
         dataSourceBuilder.EnableDynamicJson();
