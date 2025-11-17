af067fa90e29809a9b53f248ba5a2e665af4f028
BlagojeP <blagojepanic9@gmail.com>
2025-11-17 21:08:42 +0100

    feat: add account service, DTOs and repository interface

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/AccountDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class AccountDto
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public bool IsBlocked { get; set; }
+    }
+}
--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Mappers/StakeholderProfile.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Mappers/StakeholderProfile.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.Core.Domain;
 
 namespace Explorer.Stakeholders.Core.Mappers;
 
@@ -6,5 +8,7 @@ public class StakeholderProfile : Profile
 {
     public StakeholderProfile()
     {
+        CreateMap<Account, AccountDto>();
+        CreateMap<AccountCreateDto, Account>();
     }
 }
