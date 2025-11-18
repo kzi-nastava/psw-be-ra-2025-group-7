1ab6c4e124b9f57ce2280f2973f47944556f0a48
Gio <ognjen.vujanic03@gmail.com>
2025-11-18 14:43:51 +0100

    Jos manjih izmena

--- src/Modules/Tours/Explorer.Tours.Core/Domain/RepositoryInterfaces/ITourProblemRepository.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/RepositoryInterfaces/ITourProblemRepository.cs
@@ -11,6 +11,7 @@ namespace Explorer.Tours.Core.Domain.RepositoryInterfaces
     {
         PagedResult<TourProblem> GetByTourist(int touristId,int pageNumber, int pageSize);
         TourProblem Get(int id);
+        PagedResult<TourProblem> GetAll(int pageNumber, int pageSize);
         TourProblem Create(TourProblem tourProblem);
         TourProblem Update(TourProblem tourProblem);
         void Delete(int id);
