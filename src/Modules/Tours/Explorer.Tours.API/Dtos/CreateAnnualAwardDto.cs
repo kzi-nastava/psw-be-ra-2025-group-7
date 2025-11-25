54857493a31794912adf661a5a434b94cd8d912a
Aleksa1711 <doroslovac.ra9.2022@uns.ac.rs>
2025-11-25 21:35:11 +0100

    feat: Add AnnualAward service with CRUD operations

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/CreateAnnualAwardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Explorer.Tours.API.Dtos;
+
+public class CreateAnnualAwardDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Year { get; set; }
+    public DateTime VotingStartDate { get; set; }
+    public DateTime VotingEndDate { get; set; }
+}
--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -8,16 +8,29 @@ public class ToursProfile : Profile
 {
     public ToursProfile()
     {
+        // Existing mappings
         CreateMap<EquipmentDto, Equipment>().ReverseMap();
+
         CreateMap<TourProblemDto, TourProblem>()
            .ForMember(d => d.Category, opt => opt.MapFrom(s => Enum.Parse<ProblemCategory>(s.Category, true)))
            .ForMember(d => d.Priority, opt => opt.MapFrom(s => Enum.Parse<ProblemPriority>(s.Priority, true)));
+
         CreateMap<TourProblem, TourProblemDto>()
             .ForMember(d => d.Category, opt => opt.MapFrom(s => s.Category.ToString()))
             .ForMember(d => d.Priority, opt => opt.MapFrom(s => s.Priority.ToString()));
+
         CreateMap<FacilityDto, Facility>().ReverseMap();
         CreateMap<TourDto, Tour>().ReverseMap();
         CreateMap<TouristEquipment, TouristEquipmentDto>().ReverseMap();
 
+
+        CreateMap<AnnualAward, AnnualAwardDto>()
+            .ForMember(d => d.Status, opt => opt.MapFrom(s => s.Status.ToString()));
+        CreateMap<AnnualAwardDto, AnnualAward>()
+            .ForMember(d => d.Status, opt => opt.MapFrom(s => Enum.Parse<AwardStatus>(s.Status, true)));
+        CreateMap<CreateAnnualAwardDto, AnnualAward>()
+            .ForMember(d => d.Status, opt => opt.Ignore()); 
+        CreateMap<UpdateAnnualAwardDto, AnnualAward>()
+            .ForMember(d => d.Status, opt => opt.Ignore()); 
     }
 }
