7dfa8c0b47df1ece17271e5adfc537e688fabd4d
Marina Savic <marinasaviic@gmail.com>
2025-12-03 17:35:26 +0100

    fix: corrected test data, all tests pass

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
@@ -3,6 +3,5 @@ DELETE FROM stakeholders."Messages";
 DELETE FROM stakeholders."UserProfiles";
 DELETE FROM stakeholders."People";
 DELETE FROM stakeholders."Clubs";
-DELETE FROM stakeholders."Monuments";
 DELETE FROM stakeholders."Users";
 DELETE FROM stakeholders."Accounts";
