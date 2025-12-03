52aefa8e87a2eca87c6cb51de134db3426528cb5
Ognjen Damnjanovic <ognjendamnjanovic11@gmail.com>
2025-12-03 17:35:28 +0100

    fix: changes in scripts

--- src/Modules/Blog/Explorer.Blog.Tests/TestData/b-insert-blogposts.sql
+++ src/Modules/Blog/Explorer.Blog.Tests/TestData/b-insert-blogposts.sql
@@ -3,7 +3,7 @@
 VALUES
     (-1, -21, 'Test blog', 'Ovo je test blog post iz TestData skripte.', NOW(), 0, NULL);
 
-INSERT INTO blog."BlogVote" ("Id", "UserId", "Value", "VotedAt", "BlogPostsId")
+INSERT INTO blog."BlogVotes" ("Id", "UserId", "Value", "VotedAt", "BlogPostsId")
 VALUES
     (-1, -21, 1, NOW(), -1),
     (-2, -22, -1, NOW(), -1);
--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
@@ -1,8 +1,7 @@
-DELETE FROM stakeholders."Accounts";
-DELETE FROM stakeholders."Messages";
 DELETE FROM stakeholders."Reviews";
-DELETE FROM stakeholders."Monuments";
-DELETE FROM	stakeholders."TourPreferences";
+DELETE FROM stakeholders."Messages";
 DELETE FROM stakeholders."UserProfiles";
 DELETE FROM stakeholders."People";
+DELETE FROM	stakeholders."TourPreferences";
 DELETE FROM stakeholders."Users";
+DELETE FROM stakeholders."Accounts";
