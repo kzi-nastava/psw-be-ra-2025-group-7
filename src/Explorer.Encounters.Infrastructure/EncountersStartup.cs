38dbafe69b10d7df680a7a6c820286f868f2c4c7
Sara <saraponjevic02@gmail.com>
2025-12-18 22:56:16 +0100

    feat: dodavanje novog modula Encounters

---
+++ src/Explorer.Encounters.Infrastructure/EncountersStartup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Infrastructure;
+
+public static class EncountersStartup
+{
+    public static IServiceCollection ConfigureEncountersModule(this IServiceCollection services)
+    {
+        
+       // services.AddAutoMapper(typeof(EncountersProfile).Assembly);   // Preduslov da imamo ovu liniju koda je da smo definisali već Profile klasu u Core/Mappers
+        SetupCore(services);
+
+        SetupCore(services);
+        SetupInfrastructure(services);
+        return services;
+    }
+
+    private static void SetupCore(IServiceCollection services)
+    {
+       
+        // services.AddScoped<IEncounterService, EncounterService>();
+    }
+
+    private static void SetupInfrastructure(IServiceCollection services)
+    {
+        
+    }
+}
