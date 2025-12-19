46ccf782802ed893738a67ffe3ec1be494b677f3
Mile Vuletic <vuletic.ra45.2022@uns.ac.rs>
2025-12-19 09:59:50 -0800

    chore: create new Payments module boilerplate

---
+++ src/Explorer.Payments.Core/Mappers/PaymentsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Core.Mappers
+{
+    public class PaymentsProfile : Profile
+    {
+        public PaymentsProfile()
+        {
+            // Definišemo mape između domena i DTO-ova ili drugih objekata
+        }
+    }
+}
---
+++ src/Explorer.Payments.Infrastructure/PaymentsStartup.cs
@@ -0,0 +1,29 @@
+﻿using Explorer.BuildingBlocks.Infrastructure.Database;
+using Explorer.Payments.Core.Mappers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace Explorer.Payments.Infrastructure
+{
+    public static class PaymentsStartup
+    {
+        public static IServiceCollection ConfigurePaymentsModule(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(PaymentsProfile).Assembly); // Preduslov da imamo ovu liniju koda je da smo definisali već Profile klasu u Core/Mappers
+            SetupCore(services);
+            SetupInfrastructure(services);
+            return services;
+        }
+
+        private static void SetupCore(IServiceCollection services)
+        {
+            // Ovde registrujemo sve servise koje imamo u Core sloju
+        }
+
+        private static void SetupInfrastructure(IServiceCollection services)
+        {
+            // Ovde registrujemo sve repozitorijume koje imamo u Infrastructure sloju
+        }
+    }
+}
