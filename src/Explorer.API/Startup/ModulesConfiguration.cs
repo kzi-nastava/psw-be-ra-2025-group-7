633b104ec707ff9adf848dae63d4ef3a2e8932fa
Teodora <teodora.lazarevic251103@gmail.com>
2025-12-22 21:21:04 +0100

    Notes module added

--- src/Explorer.API/Startup/ModulesConfiguration.cs
+++ src/Explorer.API/Startup/ModulesConfiguration.cs
@@ -2,6 +2,7 @@ using Explorer.Blog.Infrastructure;
 using Explorer.Stakeholders.Infrastructure;
 using Explorer.Tours.Infrastructure;
 using Explorer.Encounters.Infrastructure;
+using Explorer.Notes.Infrastructure;
 
 namespace Explorer.API.Startup;
 
@@ -13,6 +14,7 @@ public static class ModulesConfiguration
         services.ConfigureToursModule();
         services.ConfigureBlogModule();
         services.ConfigureEncountersModule();
+        services.ConfigureNotesModule();
 
         return services;
     }
