a6e467ce795256d1d34ab12b4e4754be46294232
Mile Vuletic <vuletic.ra45.2022@uns.ac.rs>
2025-12-26 07:10:20 -0800

    fix: project hierarchy in solution

--- src/Explorer.Payments.Infrastructure/Migrations/20251226150537_AddWallets.Designer.cs
+++ src/Explorer.Payments.Infrastructure/Migrations/20251226150537_AddWallets.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Explorer.Payments.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Explorer.Payments.Infrastructure.Migrations
 {
     [DbContext(typeof(PaymentsContext))]
-    partial class PaymentsContextModelSnapshot : ModelSnapshot
+    [Migration("20251226150537_AddWallets")]
+    partial class AddWallets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
