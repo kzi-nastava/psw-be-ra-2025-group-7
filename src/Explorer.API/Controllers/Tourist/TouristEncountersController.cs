bc7a79d4463c266b111142c337224c12bef80473
kristijan.djapic11@gmail.com <kristijan.djapic11@gmail.com>
2026-01-20 20:26:22 +0100

    Resavanje izazova tokom tour execution

--- src/Explorer.API/Controllers/Tourist/TouristEncountersController.cs
+++ src/Explorer.API/Controllers/Tourist/TouristEncountersController.cs
@@ -117,5 +117,24 @@ namespace Explorer.API.Controllers.Tourist
             return Ok(result);
         }
 
+        [HttpPost("{encounterId:long}/activate-misc")]
+        public IActionResult ActivateMiscEncounter(long encounterId)
+        {
+            try
+            {
+                var userId = User.PersonId();
+                _encounterProgressService.ActivateMiscEncounter(encounterId, userId);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
